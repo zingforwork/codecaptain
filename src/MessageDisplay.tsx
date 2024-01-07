5ac07facd242f0aad4e63c01c04da8818cbf15dc
Tadthep Kanlayanapark <virginzing@gmail.com>
2024-01-07 16:38:17 +0700

    added code highlight

--- src/MessageDisplay.tsx
+++ src/MessageDisplay.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 interface MessageDisplayProps {
   message: string;
 }
 
 const MessageDisplay: React.FC<MessageDisplayProps> = ({ message }) => {
+  const components = {
+    code: ({ node, inline, className, children, ...props }: any) => {
+      const match = /language-(\w+)/.exec(className || '');
+      return !inline && match ? (
+        <SyntaxHighlighter style={tomorrow} language={match[1]} PreTag="div" {...props}>
+          {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
+      );
+    },
+  };
+
   return (
     <div>
-      <ReactMarkdown>{message}</ReactMarkdown>
+      <ReactMarkdown components={components}>{message}</ReactMarkdown>
     </div>
   );
-}
+};
 
 export default MessageDisplay;
